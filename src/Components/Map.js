4d0dec70fc1f62617d759f9d94c85ba8ef59b669
Dwi Satria <dwisatriakusuma4@gmail.com>
2019-01-08 07:34:44 +0700

    setup react native map with google api

---
+++ src/Components/Map.js
@@ -0,0 +1,27 @@
+// Library Import
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+// Component & Styles Import
+import SilverMapStyle from '../MapStyles/SilverMapStyle.json'
+
+export default class Map extends React.PureComponent {
+  render() {
+    return (
+      <MapView 
+        provider = { PROVIDER_GOOGLE }
+        style = { styles.container }
+        customMapStyle = { SilverMapStyle }
+        initialRegion = {{
+          latitude: -6.976671,
+          longitude: 107.633129,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.05,
+        }}
+      />
+    );
+  }
+}
+
+ 
